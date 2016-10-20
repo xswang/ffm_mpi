c73026dd863decdc31094622e55807b95eea1db7
wangxiaoshu <wangxiaoshu@yidian-inc.com>
2016-10-20 21:28:06 +0800

    multithread gradient calculate

--- src/learner/ftrl_learner.h
+++ src/learner/ftrl_learner.h
@@ -2,6 +2,8 @@
 #include "learner.h"
 #include "../predict.h"
 #include <cblas.h>
+#include <thread>
+#include <mutex>
 
 namespace DML{
 class FTRL_learner : public Learner{
@@ -49,13 +51,12 @@ class FTRL_learner : public Learner{
             }
             md.close();
         }
-
-        void batch_gradient_calculate(){
+        void batch_gradient_calculate_multithread(int thread_batch_size){
+            std::cout<<"my rank = "<<rank<<std::endl;
+                if(rank == 0)std::cout<<"hhhhhhhhh"<<std::endl;
             int group = 0, index = 0; float value = 0.0, pctr = 0.0;
-            memset(loc_g, 0.0, param->fea_dim);//notation:
-            memset(loc_g_v, 0.0, v_dim);//notation: 
-
-            for(int line = 0; line < param->batch_size; line++){
+            for(int line = 0; line < thread_batch_size; line++){
+                if(rank == 0)std::cout<<" line ==== "<<line<<std::endl;
                 float wx = bias;
                 int ins_seg_num = data->fea_matrix[row].size();
                 std::vector<float> vx_sum(param->factor, 0.0);
@@ -81,13 +82,78 @@ class FTRL_learner : public Learner{
                 }//end for
                 for(int k = 0; k < param->factor; k++){
                     if(param->islr) break;
-                    vxvx += vx_sum[k] * vx_sum[k]; 
+                    vxvx += vx_sum[k] * vx_sum[k];
                 }
                 vxvx -= vvxx;
                 wx += vxvx * 1.0 / 2.0;
                 pctr = sigmoid(wx);
                 float delta = pctr - data->label[row];
+                for(int col = 0; col < ins_seg_num; col++){
+                    group = data->fea_matrix[row][col].fgid;
+                    index = data->fea_matrix[row][col].fid;
+                    value = data->fea_matrix[row][col].val;
+                    mutex.lock();
+                    loc_g[index] += delta * value;
+                    mutex.unlock();
+                    float vx = 0.0;
+                    for(int k = 0; k < param->factor; k++){
+                        if(param->islr) break;
+                        for(int f = 0; f < param->group; f++){
+                            setIter = cross_field[group].find(f);
+                            if(setIter == cross_field[group].end()) continue;
+                            if(param->isfm) f = 0;
+                            float tmpv = getElem(loc_v, k, index, f);
+                            vx = tmpv * value;
+                            mutex.lock();
+                            addVal(loc_g_v, -1 * delta * (vx_sum[k] - vx) * value, k, index, f);
+                            mutex.unlock();
+                            if(param->isfm) break;
+                        }
+                    }
+                }
+                mutex.lock();
+                row++;
+                if(rank == 0)std::cout<<"row ==== "<<row<<std::endl;
+                mutex.unlock();
+            }//end for
+        }//end batch_gradient_calculate_multithread
 
+        void batch_gradient_calculate(){
+                int group = 0, index = 0; float value = 0.0, pctr = 0.0;
+                memset(loc_g, 0.0, param->fea_dim);//notation:
+                memset(loc_g_v, 0.0, v_dim);//notation: 
+                for(int line = 0; line < param->batch_size; line++){
+                        float wx = bias;
+                        int ins_seg_num = data->fea_matrix[row].size();
+                        std::vector<float> vx_sum(param->factor, 0.0);
+                        float vxvx = 0.0, vvxx = 0.0;
+                        std::set<int>::iterator setIter;
+                        for(int col = 0; col < ins_seg_num; col++){//for one instance
+                                group = data->fea_matrix[row][col].fgid;
+                                index = data->fea_matrix[row][col].fid;
+                                value = data->fea_matrix[row][col].val;
+                                wx += loc_w[index] * value;
+                                for(int k = 0; k < param->factor; k++){
+                                        if(param->islr) break;
+                                        for(int f = 0; f < param->group; f++){
+                                                setIter = cross_field[group].find(f);
+                                                if(setIter == cross_field[group].end()) continue;
+                                                if(param->isfm) f = 0;
+                                                int loc_v_temp = getElem(loc_v, k, index, f);
+                                                vx_sum[k] += loc_v_temp * value;
+                                                vvxx += loc_v_temp * loc_v_temp * value * value;
+                                                if(param->isfm) break;
+                                        }
+                                }
+                        }//end for
+                        for(int k = 0; k < param->factor; k++){
+                                if(param->islr) break;
+                                vxvx += vx_sum[k] * vx_sum[k]; 
+                        }
+                        vxvx -= vvxx;
+                        wx += vxvx * 1.0 / 2.0;
+                pctr = sigmoid(wx);
+                float delta = pctr - data->label[row];
                 for(int col = 0; col < ins_seg_num; col++){
                     group = data->fea_matrix[row][col].fgid;
                     index = data->fea_matrix[row][col].fid;
@@ -109,7 +175,7 @@ class FTRL_learner : public Learner{
                 }
                 row++;
             }//end for
-        }
+        }//end batch_gradient_calculate
 
         void allreduce_gradient(){
             cblas_dscal(param->fea_dim, 1.0/param->batch_size, loc_g, 1);
@@ -270,11 +336,26 @@ class FTRL_learner : public Learner{
                 row = 0;
                 int batches = 0;
                 std::cout<<"epoch "<<epoch<<" ";
+                std::cout<<"rankkkkkk is ====================== "<<rank<<std::endl;
                 pred->run(loc_w, loc_v);
+                std::cout<<" is ====================== "<<rank<<std::endl;
                 if(rank == 0 && (epoch+1) % 20 == 0) dump(epoch);
                 while(row < data->fea_matrix.size()){
                     if( (batches == batch_num_min - 1) ) break;
-                    batch_gradient_calculate();
+                    memset(loc_g, 0.0, param->fea_dim);//notation:
+                    memset(loc_g_v, 0.0, v_dim);//notation:
+                    //batch_gradient_calculate();
+                    int core_num = std::thread::hardware_concurrency();
+                    std::vector<std::thread> threads;
+                    int thread_batchsize = param->batch_size / core_num;
+                    ///* 
+                    for(int i = 0; i < core_num; i++){
+                        threads.push_back(std::thread(&FTRL_learner::batch_gradient_calculate_multithread, this, thread_batchsize));
+                    }
+                    for(auto &t : threads){
+                        t.join();
+                    }
+                    //*/
                     if(row % 200000 == 0) std::cout<<"row = "<<row<<std::endl;
                     allreduce_gradient();
                     allreduce_weight();
@@ -284,6 +365,7 @@ class FTRL_learner : public Learner{
         }
 
     public:
+        std::mutex mutex;
         int row;
         int loc_g_nonzero;
         int loc_g_v_nonzero;
